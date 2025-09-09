e2a64ffff4e7e5a901f5a2b75af0f7e8e08e73fe
Sne2109 <159558700+Sne2109@users.noreply.github.com>
2025-09-09 21:41:57 +0530

    Create char.java

---
+++ char.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user for input
+        
+        char ch = scanner.next().charAt(0);
+
+        // Print the entered character
+        System.out.println(ch);
+
+        scanner.close();
+    }
+}
