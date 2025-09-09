f488223468c80f7884416c45882f80cbab9430eb
Sne2109 <159558700+Sne2109@users.noreply.github.com>
2025-09-09 21:55:19 +0530

    Create ternary.java

---
+++ ternary.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int a;
+        a=sc.nextInt();
+        System.out.println(a != 0 ? (a > 0 ? "+" : "-") + Math.abs(a) : "0");
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
